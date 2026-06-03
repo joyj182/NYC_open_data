4e9ad7cf363ba8a568144d77f87c51624d11cbca
joyj182 <joyj18@nycstudents.net>
2026-06-03 18:08:56 +0000

    rfnsldjla

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ async function init(){
   for(let i = 0; i < data.length; i+=1){
     let arrest = data[i];
     build += `<div class="fitted card">
-                <h1> ${arrest.ofns_desc }</h1>
+                <h1>${arrest.ofns_desc }</h1>
                 <h3>${arrest.arrest_key}</h3>
                 <hr>
                 <p>${arrest.perp_sex}</p>
                 <p>${arrest.perp_race} </p>
-                <p> ${arrest.age_group}</p>
+                <p>${arrest.age_group}</p>
                 <hr>
                 <p>${arrest.law_code}</p>
               </div>` ;   
