a1e61076cbe1b69a2f049213fb95c599f1b28c15
joyj182 <joyj18@nycstudents.net>
2026-06-07 00:07:46 +0000

    sdasdas

--- script.js
+++ script.js
@@ -52,3 +52,31 @@ function filterByGenderRace(){
   result.innerHTML = `${ct} Results found.`;
   output.innerHTML = build; 
 }
+/*
+function searchByAge(){
+    let age = document.getElementById("age").value;
+    let result = document.getElementById("result");
+  
+    let build = "";
+    let ct = 0;
+
+    for(let i = 0; i < data.length; i+=1){
+    let arrest = data[i];
+    if(arrest.perp_sex == g && arrest.perp_race == r){
+      build += `<div class="fitted card">
+                <h1>${arrest.ofns_desc}</h1>
+                <h3>${arrest.arrest_key}</h3>
+                <hr>
+                <p>${arrest.perp_sex}</p>
+                <p>${arrest.perp_race} </p>
+                <p>${arrest.age_group}</p>
+                <hr>
+                <p>${arrest.law_code}</p>
+              </div>` ; 
+      ct += 1;        
+    }
+  }
+  result.innerHTML = `${ct} Results found.`;
+  output.innerHTML = build; 
+}
+    */
